fe39be038f0a914b49617ab1f51b132d8eddf3db
Soha Refaat <144360872+soha-Refaat@users.noreply.github.com>
2024-01-03 04:37:28 -0800

    Add files via upload

---
+++ js/music.js
@@ -0,0 +1,37 @@
+let allMusic = [
+    {
+        name: "First Song",
+        artist: "First Singer",
+        img: "images/image1.jpg",
+        src: "oudio/1.mp3"
+
+    },
+    {
+        name: "Second Song",
+        artist: "Second Singer",
+        img: "images/image2.jpg",
+        src: "oudio/2.mp3"
+
+    },
+    {
+        name: "Third Song",
+        artist: "Third Singer",
+        img: "images/image3.jpg",
+        src: "oudio/3.mp3"
+
+    },
+    {
+        name: "Fourth Song",
+        artist: "Fourth Singer",
+        img: "images/image4.jpg",
+        src: "oudio/4.mp3"
+
+    },
+    {
+        name: "Fifth Song",
+        artist: "Fifth Singer",
+        img: "images/image5.jpg",
+        src: "oudio/5.mp3"
+
+    }
+];
